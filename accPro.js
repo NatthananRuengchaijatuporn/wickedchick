9329acff8bd5a94d02e8fa7df85b8db45e212640
Natthanan Ruengchaijatuporn <northlnw@hotmail.com>
2018-11-25 08:47:34 +0700

    add .then in accPro.js to change user name

--- accPro.js
+++ accPro.js
@@ -5,6 +5,10 @@ var button = document.querySelector("#button-addon2");
 
 button.addEventListener("click", function(){
 	console.log("Hello")
-	axios.post('http://localhost:3000/user',changeName);
+	axios.post('http://localhost:3000/user',changeName).then(function(response){
+		document.getElementById('productName').innerHTML = changeName;
+	}).catch(function(error)}{
+		console.log(error);
+	})
 	//axios.post('http://localhost:3000/basket',{test:'Yeah'})
 });
