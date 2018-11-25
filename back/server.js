ad5aaf3dadf9b2b33fb0e600cd3f55357aa94667
unknown <magician_TH@hotmail.com>
2018-11-25 22:59:45 +0700

    Push INSERT #not test yet battery is low

--- back/server.js
+++ back/server.js
@@ -9,8 +9,8 @@ app.use(cors());
 var conn = mysql.createConnection({
 	host:"localhost",
 	user:"root",
-	password:"insert password mung si",
-	database:"e useless 2 tua yak don kicked pow sardddddddd",
+	password:"tunlnw",
+	database:"wickedchick",
 	port:3306
 });
 /*conn.connect(function(err) {
@@ -20,35 +20,36 @@ var conn = mysql.createConnection({
 app.get('/product',(req,res)=>{
 	var idkub = req.query.id;
 	console.log(" idkub = "+idkub+" AND type of idkub is = " +typeof idkub);
-	var txt = 'SELECT i.ItemID,i.ItemName,i.ItemDescription,user.DisplayName, FROM item as i INNER JOIN user ON i.SUsername=user.Username where i.ItemID = '+idkub+';';
+	var txt = 'SELECT i.ItemID,i.ItemName,i.ItemDescription,user.DisplayName FROM item as i INNER JOIN user ON i.SUsername=user.Username where i.ItemID = '+idkub+';';ผัั
 	conn.query(txt, function(err,results){
 		if (err) console.error(err);
-		console.log(results);
 		res.json(results)
 	});
 });
 
-app.get('/pick',(req,res)=>{
-/*
-	conn.query('SELECT * FROM account',function(err,response){
-		if(err)throw err;
-		response.forEach(function(element){console.log(element.account_number)})
-		BUsername,ItemID,timestamp,Quantity
-		res.send(response);
-	});
-*/
-	console.log("I got it.")
-});
-//add into 
 app.post('/cart',(req,res)=>{
-	console.log(req.body)
-	//console.log(req.B)
-	//conn.query('INSERT INTO SEND_TO VALUES (ItemID) VALUES ('+req.ItemID+')',function(err,response){
-	//	if(err)throw err;
-	//})
-	//must get price first (sum of all price)
-
-	//conn.query('INSERT INTO PURCHASEDORDER VALUES (OrderID)')
+	var items = req.body.listOfProduct;
+	console.log(items)
+	var totalPrice = 0;
+	var insertValuesTxt = '';
+	for (i = 0; i< items.length; i++ ){
+		totalPrice += items[i].Price;
+		insertValuesTxt += '('+items[i].ItemID+', 322)'; // 4 is OrderID
+		if ( i < items.length -1 ) {
+		insertValuesTxt += ',';
+		}
+	}
+	console.log(insertValuesTxt);
+	console.log('total Price kub '+totalPrice);
+	var datetime = new Date().toISOString().slice(0, 19).replace('T', ' ');
+	var txtkub = "INSERT INTO purchase_order (OrderID, TotalPrice, Orderdate, CouponID) VALUES (322, "+totalPrice+", '"+datetime+"', NULL);";
+	/*conn.query(txtkub, function(err,response){
+		if(err)throw err;
+	})
+	conn.query('INSERT INTO SENT_TO (ItemID,OrderID) Values '+insertValuesTxt+';', function(err,response){
+		if(err)throw err;
+	})*/
+	
 });
 app.get('/home', (req,res) => {
 	//conn.query('SELECT itemid,itemname FROM ITEM')
